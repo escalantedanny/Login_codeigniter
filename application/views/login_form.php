4f597291aaeb9943fa0a0d53cc1a6232d2055db2
Danny Escalante/Neoris <danny.escalante@neoris.com>
2018-06-12 17:40:54 -0400

    login de usuario codeigniter con base de datos mysql

---
+++ application/views/login_form.php
@@ -0,0 +1,54 @@
+
+
+<html>
+<?php
+if (isset($this->session->userdata['logged_in'])) {
+
+header("location: http://localhost/login/index.php/user_authentication/user_login_process");
+}
+?>
+<head>
+<title>Administrador usuario</title>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/css/style.css">
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<?php
+if (isset($logout_message)) {
+echo "<div class='message'>";
+echo $logout_message;
+echo "</div>";
+}
+?>
+<?php
+if (isset($message_display)) {
+echo "<div class='message'>";
+echo $message_display;
+echo "</div>";
+}
+?>
+<div id="main">
+<div id="login">
+<h2>Administrador de usuario</h2>
+<hr/>
+<?php echo form_open('user_authentication/user_login_process'); ?>
+<?php
+echo "<div class='error_msg'>";
+if (isset($error_message)) {
+echo $error_message;
+}
+echo validation_errors();
+echo "</div>";
+?>
+<label>Usuario :</label>
+<input type="text" name="username" id="name" placeholder="username"/><br /><br />
+<label>Contraseña :</label>
+<input type="password" name="password" id="password" placeholder="**********"/><br/><br />
+<input type="submit" value=" Login " name="submit"/><br />
+<a href="<?php echo base_url() ?>index.php/user_authentication/user_registration_show">Registrarse</a>
+<?php echo form_close(); ?>
+</div>
+</div>
+</body>
+</html>
+
---
+++ asset/database/login.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-06-2018 a las 23:38:33
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_login`
+--
+
+CREATE TABLE `user_login` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `user_login`
+--
+ALTER TABLE `user_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
